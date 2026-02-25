a0d979362b59a10591fdf6595ca62da24f849821
Katuri Mohan kumar <mohankumarkaturi1@gmail.com>
2026-02-25 13:51:47 +0530

    Delete 01-user-service directory

--- 01-user-service/src/main/java/com/codegnan/repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.codegnan.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.codegnan.models.User;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-
-}
